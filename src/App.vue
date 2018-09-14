2d3de8f70734d9a42be4599882b15973f8081b35
Aritha Hettiaratchi <arithah@gmail.com>
2018-09-14 13:12:36 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      title: 'Todo List'
+    }
+  }
+}
+</script>
+
+<style>
+h1 {
+  color: #444;
+  font-weight: normal;
+  text-align: center;
+}
+</style>
