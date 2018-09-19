6bbc3002d504587442644ef72962815b1dc8c409
Aritha Hettiaratchi <arithah@gmail.com>
2018-09-19 11:30:13 -0400

    responsive fix

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -3,11 +3,19 @@
         <router-link :to="{ name: 'TodayList' }" class="brand-logo center">
             Todo List
         </router-link>
-        <ul class="right">
+        <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+        <ul class="right hide-on-med-and-down">
             <li v-if="!user"><router-link :to="{ name: 'Signup' }">Signup</router-link></li>
             <li v-if="!user"><router-link :to="{ name: 'Login' }">Login</router-link></li>
             <li v-if="user"><router-link :to="{ name: 'ViewProfile' }">{{ user.email }}</router-link></li>
             <li v-if="user"><a @click="logout">Logout</a></li>
+        </ul>
+
+        <ul class="sidenav" id="mobile-demo">
+          <li v-if="!user"><router-link :to="{ name: 'Signup' }">Signup</router-link></li>
+          <li v-if="!user"><router-link :to="{ name: 'Login' }">Login</router-link></li>
+          <li v-if="user"><router-link :to="{ name: 'ViewProfile' }">{{ user.email }}</router-link></li>
+          <li v-if="user"><a @click="logout">Logout</a></li>
         </ul>
     </nav>
 </template>
