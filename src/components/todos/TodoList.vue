4d1dbefa500b2db4d480f25a8aefede5e85991c5
Aritha Hettiaratchi <arithah@gmail.com>
2018-10-02 22:46:29 +0100

    WIP - mobile expand todo for functions

--- src/components/todos/TodoList.vue
+++ src/components/todos/TodoList.vue
@@ -1,15 +1,31 @@
 <template>
   <div class="todo-list">
-    <p v-if="showEmptyMessage()" class="grey-text">{{ emptyMessage }}</p>
-    <div class="todo">
-      <p v-for="(todo, index) in todoList" :key="todo.id">
-        <label v-if="status === todo.status">
-          <input type="checkbox" @click="todoClick(todo, index)" class="filled-in" :checked="todo.status === 'completed' ? 'checked' : ''"/>
-          <span :class="todo.status">{{ todo.displayName }}</span>
-          <i class="material-icons delete right" @click="deleteTodo(todo, index)">delete</i>
-          <i class="material-icons edit right" @click.prevent="editTodo(todo)">edit</i>
-        </label>
-      </p>
+    <div class="desktop hide-on-med-and-down">
+      <p v-if="showEmptyMessage()" class="grey-text">{{ emptyMessage }}</p>
+      <div class="todo">
+        <p v-for="(todo, index) in todoList" :key="todo.id">
+          <label v-if="status === todo.status">
+            <input type="checkbox" @click="todoClick(todo, index)" class="filled-in" :checked="todo.status === 'completed' ? 'checked' : ''"/>
+            <span :class="todo.status">{{ todo.displayName }}</span>
+            <i class="material-icons delete right" @click="deleteTodo(todo, index)">delete</i>
+            <i class="material-icons edit right" @click.prevent="editTodo(todo)">edit</i>
+          </label>
+        </p>
+      </div>
+    </div>
+    <div class="mobile hide-on-large-only">
+      <p v-if="showEmptyMessage()" class="grey-text">{{ emptyMessage }}</p>
+      <div class="todo">
+        <p v-for="(todo, index) in todoList" :key="todo.id">
+          <label v-if="status === todo.status">
+            <input type="checkbox" @click="todoClick(todo, index)" class="filled-in" :checked="todo.status === 'completed' ? 'checked' : ''"/>
+            <span :class="todo.status" @click.prevent="expandTodo(todo, index)">{{ todo.displayName }}</span>
+            <div v-if="expandedTodo === index"  class="expanded">
+              Expanded
+            </div>
+          </label>
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -19,7 +35,9 @@
 export default {
   name: 'TodoList',
   data () {
-    return {}
+    return {
+      expandedTodo: null
+    }
   },
   props: [ 'todoList', 'status', 'emptyMessage' ],
   methods: {
@@ -31,6 +49,13 @@ export default {
     },
     deleteTodo (todo, index) {
       this.$emit('deleteTodo', { todo, index })
+    },
+    expandTodo (todo, index) {
+      if (this.expandedTodo === index) {
+        this.expandedTodo = null
+      } else {
+        this.expandedTodo = index
+      }
     },
     showEmptyMessage () {
       const list = this.todoList.filter(todo => {
@@ -49,13 +74,13 @@ export default {
   .heading {
     padding-bottom: 25px;
   }
-  .todo-list .todo p {
+  .todo-list .desktop .todo p {
     text-align: left;
   }
   .todo-list .todo .completed {
     text-decoration-line: line-through;
   }
-  .todo-list .todo .edit {
+  .todo-list .desktop .todo .edit {
     margin: 0px 2px;
     cursor: pointer;
   }
@@ -64,12 +89,12 @@ export default {
     cursor: pointer;
   }
 
-  @media only screen and (max-width: 600px) {
+  /* @media only screen and (max-width: 600px) {
     .todo-list .todo p label span {
       width: 230px;
       white-space: nowrap;
       overflow: hidden;
       text-overflow: ellipsis;
     }
-  }
+  } */
 </style>
